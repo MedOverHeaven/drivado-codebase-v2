2da692fce6a347fcad48384b4fb18f428bd7af98
MedOverHeaven <mohmaed.ali.salek.02@gmail.com>
2026-06-06 13:20:23 +0100

    fix AgencyFactory not found

--- tests/Feature/DatabaseLogicTest.php
+++ tests/Feature/DatabaseLogicTest.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\Vehicle;
 use App\Models\Agency;
-use App\Models\User;
 
 class DatabaseLogicTest extends TestCase
 {
@@ -14,9 +13,25 @@ class DatabaseLogicTest extends TestCase
 
     public function test_vehicle_belongs_to_agency(): void
     {
-        $agency = Agency::factory()->create();
-        $vehicle = Vehicle::factory()->create(['agency_id' => $agency->id]);
-        
+        $agency = Agency::create([
+            'agency_name' => 'Test Agency',
+            'city' => 'Oujda',
+            'address' => '123 Rue Test',
+            'phone' => '0600000000',
+            'email' => 'test@agency.com',
+            'user_id' => 1,
+        ]);
+
+        $vehicle = Vehicle::create([
+            'agency_id' => $agency->id,
+            'make' => 'Dacia',
+            'model' => 'Logan',
+            'year' => 2023,
+            'category' => 'sedan',
+            'price_per_day' => 250,
+            'is_available' => true,
+        ]);
+
         $this->assertEquals($agency->id, $vehicle->agency->id);
     }
 }
