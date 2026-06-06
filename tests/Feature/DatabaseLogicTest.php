d94ca8205a66ed7a90bbd1c64c92dd28987db648
MedOverHeaven <mohmaed.ali.salek.02@gmail.com>
2026-06-06 13:22:32 +0100

    fix legal_id required in agency test

--- tests/Feature/DatabaseLogicTest.php
+++ tests/Feature/DatabaseLogicTest.php
@@ -13,14 +13,15 @@ class DatabaseLogicTest extends TestCase
 
     public function test_vehicle_belongs_to_agency(): void
     {
-        $agency = Agency::create([
-            'agency_name' => 'Test Agency',
-            'city' => 'Oujda',
-            'address' => '123 Rue Test',
-            'phone' => '0600000000',
-            'email' => 'test@agency.com',
-            'user_id' => 1,
-        ]);
+           $agency = Agency::create([
+    'agency_name' => 'Test Agency',
+    'city' => 'Oujda',
+    'address' => '123 Rue Test',
+    'phone' => '0600000000',
+    'email' => 'test@agency.com',
+    'legal_id' => 'RC123456',
+    'user_id' => 1,
+]);
 
         $vehicle = Vehicle::create([
             'agency_id' => $agency->id,
